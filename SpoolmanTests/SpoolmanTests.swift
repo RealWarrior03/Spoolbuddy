5f0de46d54b2030f92774f8b003dde4caacd4b5d
hkrieger <henry.krieger@campus.tu-berlin.de>
2026-01-09 15:49:54 +0100

    Initial Commit

---
+++ SpoolmanTests/SpoolmanTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpoolmanTests.swift
+//  SpoolmanTests
+//
+//  Created by Henry Krieger on 09.01.26.
+//
+
+import Testing
+
+struct SpoolmanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
